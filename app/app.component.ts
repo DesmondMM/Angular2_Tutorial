5b54ac50e959909e74095a503e2d5210c431a4dc
DesmondMM <mutsakaradesmond@gmail.com>
2016-01-29 18:58:49 -0800

    Added back ticks to the template to increase readability

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,8 +7,11 @@ interface Hero{
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2><div><label>name:' +
-    '</label> {{hero.name}}</div>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+    `
 })
 export class AppComponent {
     public title = 'Tour of Heroes'
