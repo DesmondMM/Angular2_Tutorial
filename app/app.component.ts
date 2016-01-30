c064c39bddd61d4c4e8faf2048aabec7b051f50c
DesmondMM <mutsakaradesmond@gmail.com>
2016-01-29 18:42:21 -0800

    Created a Hero project, implemented a Hero interface

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component} from 'angular2/core';
+
+interface Hero{
+    id: number;
+    name: string;
+}
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+})
+export class AppComponent {
+    public title = 'Tour of Heroes'
+    public hero: Hero = {
+        id:1,
+        name: 'windstorm'
+    };
+}
